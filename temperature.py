1e835080d3c7a98c7732ae69cc6706e1979eb032
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-06-26 14:40:21 +0200

    temperature ok

--- temperature.py
+++ temperature.py
@@ -1,13 +1,15 @@
 nb_total_mesures = int(input())
 temp_min = int(input())
 temp_max = int(input())
-nb_mesures = 0
 
-temp_mesuree = int(input())
-while temp_mesuree <= temp_max and temp_mesuree >= temp_min   
-    ifnb_mesures+1 != nb_total_mesures:
-    print("Rien à signaler")
+nb_mesures = 0
+temp_valide = True
+
+while temp_mesuree <= temp_max and temp_mesuree >= temp_min :  
+    if nb_mesures+1 != nb_total_mesures: 
+        print("Rien à signaler")
     nb_mesures += 1
     temp_mesuree = int(input())
 print("Alerte !!")
+
 
