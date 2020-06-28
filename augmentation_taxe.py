1471e3b0e31bb507a20e9ed6d35031f22794ad66
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-06-28 09:56:30 +0200

    augmentation taxes ok

--- augmentation_taxe.py
+++ augmentation_taxe.py
@@ -1,4 +1,9 @@
 taxe_actuelle = float(input())
 nouvelle_taxe = float(input())
-prix = float(input())
+prix_ttc = float(input())
 
+prix_ht = round(prix_ttc) / (5/100+1)
+
+nouveau_prix = round(prix_ht) * nouvelle_taxe(100+1)*100
+
+print(round(nouveau_prix))
