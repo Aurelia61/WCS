44c5a943d3e2028507041b3c6c7c5300c103df05
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-02 16:28:42 +0200

    visite mine ok

--- temp.py
+++ temp.py
@@ -1,2 +1,3 @@
-poids = 7.0//6
-print (poids)
+numero = [0,1,2,3,4,5]
+for num in range(6,0, -1):
+   print(numero[num-1])
--- visite_mine.py
+++ visite_mine.py
@@ -9,7 +9,11 @@ for num in range(nb_deplacements):
         deplacements[num] = 2
     elif  deplacements[num] == 4 :
         deplacements[num] = 5
+    elif deplacements[num] == 2 :
+        deplacements[num] = 1
+    elif  deplacements[num] == 5 :
+        deplacements[num] = 4
 
-for num in range(0,nb_deplacements, -1):
-    print(deplacements[num])
+for num in range(nb_deplacements, 0, -1):
+    print(deplacements[num-1])
 
