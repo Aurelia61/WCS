50136e1beb26e868a7334c01ac8ccb566a9dbb0a
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-03 14:18:56 +0200

    journee cadeau in progress

--- essai.py
+++ essai.py
@@ -1,17 +1,5 @@
-nb_habitants = int(input())
-fortune_habitant = [""] * nb_habitants
-
-for habitant in range(nb_habitants):
-    fortune_habitant[habitant] = int(input())
-
-fortune_habitant.sort()
-
-if nb_habitants % 2 == 0 :
-    milieu = nb_habitants//2
-    valeur = fortune_habitant[milieu]
-else :
-    milieu = (nb_habitants-1)//2 
-    valeur = fortune_habitant[milieu]
-
-
+# print(round(10.0,2))
+# print(10/2)
+valeur = 5-0.5
+valeur = round(valeur,1)
 print(valeur)
--- journee_cadeaux.py
+++ journee_cadeaux.py
@@ -1,16 +1,19 @@
 nb_habitants = int(input())
-somme_fortune = 0
+fortune_habitant = [""] * nb_habitants
 
+for habitant in range(nb_habitants):
+    fortune_habitant[habitant] = int(input())
 
-if nb_habitants % 2 != 0:
+fortune_habitant.sort()
 
-    for habitant in range( nb_habitants) :
-        fortune = int(input())
-        somme_fortune += fortune
-    print(round(somme_fortune/nb_habitants))
-
-else:
-    for habitant in range( nb_habitants) :
-        fortune = int(input())
-        somme_fortune += fortune
-    print(round(somme_fortune/nb_habitants+1,1))
+if nb_habitants % 2 == 0 :
+    milieu = int(nb_habitants//2)
+    milieu_inf = fortune_habitant[milieu-1]
+    milieu_sup = fortune_habitant[milieu+1]
+    milieu_ok = (milieu_sup-milieu_inf)//2 + milieu_inf
+    valeur = fortune_habitant[milieu_ok]
+    print(valeur)
+else :
+    milieu = (nb_habitants-1)//2
+    valeur = fortune_habitant[milieu]
+    print(valeur)
