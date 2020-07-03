02129a621709f0ba8885da5b39542e0538a0d4b4
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-03 15:36:28 +0200

    journee cadeaux ok

--- journee_cadeaux.py
+++ journee_cadeaux.py
@@ -1,19 +1,11 @@
-nb_habitants = int(input())
-fortune_habitant = [""] * nb_habitants
-
-for habitant in range(nb_habitants):
-    fortune_habitant[habitant] = int(input())
-
-fortune_habitant.sort()
-
-if nb_habitants % 2 == 0 :
-    milieu = int(nb_habitants//2)
-    milieu_inf = fortune_habitant[milieu-1]
-    milieu_sup = fortune_habitant[milieu+1]
-    milieu_ok = (milieu_sup-milieu_inf)//2 + milieu_inf
-    valeur = fortune_habitant[milieu_ok]
-    print(valeur)
-else :
-    milieu = (nb_habitants-1)//2
-    valeur = fortune_habitant[milieu]
-    print(valeur)
+nbPersonnes = int(input())
+fortune = [0] * nbPersonnes
+for idPersonne in range(nbPersonnes):
+   fortune[idPersonne] = int(input())
+fortune.sort()
+if nbPersonnes % 2 == 1:
+   milieu = (nbPersonnes - 1) // 2
+   print( fortune[milieu] )
+else:
+   milieu = nbPersonnes // 2
+   print( ( fortune[milieu - 1] + fortune[milieu] ) / 2 )
