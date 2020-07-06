9d305aa604d7fbe36905aa59275c29ff06817f1b
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-06 10:46:39 +0200

    banquet in progress

--- banquet_principal.py
+++ banquet_principal.py
@@ -1,23 +1,19 @@
-nbPositions = int(input())
-nbChangements = int(input())
- 
-tableau = [0] * (nbPositions)
- 
-for position in range(0 , nbPositions) :
-    place = int(input())
-    tableau[position] = place
-    
-     
-for loop in range(nbChangements) :
-    changement1 = int(input())
-    changement2 = int(input())
-    changementtmp = changement1
-    
-    tableau[changement1] = tableau[changement2]
-    tableau[changement2] = changementtmp
-    
-    
- 
-for position in range(0, nbPositions):  
-    print()
-    print(tableau[position], " ")
+
+nb_positions = int(input())
+nb_changements = int(input())
+
+
+positions_initiales =[""] * nb_positions
+
+for i in range(nb_positions):
+    positions_initiales[i] = int(input())
+
+for changement in range(nb_changements) :
+    changement_1 = int(input())
+    changement_2 = int(input())
+    changement = changement_1+1
+    positions_initiales[changement_1-1] = changement_2+1
+    positions_initiales[changement_2-1] = changement
+
+print(positions_initiales)
+
