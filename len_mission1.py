129bf9a4b8fbcc756b617862c0760b7384592ece
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-07-08 16:15:17 +0200

    len ok

---
+++ len_mission1.py
@@ -0,0 +1,9 @@
+string = "Taumata whakatangihanga koauau o tamatea turi pukaka piki maungah oronuku pokai whenuaki tanatahu "
+
+
+def compter(string_def) :
+    
+    print (len(string_def.replace(" ","")))
+
+compter(string)
+print(string)
