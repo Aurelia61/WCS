286c539fce6d1912f863dd2365f49dd1ec4e9888
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-08-07 16:57:09 +0200

    jaden case ok

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
-string = "how can"
+def to_jaden_case(string):
+    string_list = string.split()
+    cap_string_list =[]
+    for word in string_list :
+        cap_word = word.capitalize()
+        cap_string_list.append(cap_word)
+    texte_jaden = " ".join(cap_string_list)
+    print(texte_jaden)
 
-string_1 = " ".join(w.capitalize() for w in string.split())
-print(string_1)
-print()
+quote = "How can mirrors be real if our eyes aren't real"  
+to_jaden_case(quote)
 
-# import string
-# string_2 = string.capwords(string)
-# print(string_2)
-# print()
-
-
-# to_jaden_case = string.capwords
-
-# print (to_jaden_case("how can"))
-# print()
 
 
 
