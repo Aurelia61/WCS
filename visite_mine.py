92217b024c7bda63375c1016f1a1983b173c03c3
Aurelia61 <67269542+Aurelia61@users.noreply.github.com>
2020-08-07 10:53:20 +0200

    ok

--- visite_mine.py
+++ visite_mine.py
@@ -7,11 +7,11 @@ for num in range(nb_deplacements) :
 for num in range(nb_deplacements):
     if deplacements[num] == 1 :
         deplacements[num] = 2
-    elif  deplacements[num] == 4 :
+    elif deplacements[num] == 4 :
         deplacements[num] = 5
     elif deplacements[num] == 2 :
         deplacements[num] = 1
-    elif  deplacements[num] == 5 :
+    elif deplacements[num] == 5 :
         deplacements[num] = 4
 
 for num in range(nb_deplacements, 0, -1):
